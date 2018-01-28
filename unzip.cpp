0858133f834209fa2157abeafdbf6fb9c03d80eb
Kevin <trank@wit.edu>
2018-01-28 18:10:53 -0500

    error checking

--- unzip.cpp
+++ unzip.cpp
@@ -27,12 +27,9 @@ int main (int argc, char* argv[])
 		string fileName = argv[1];
 		if(fileName.substr(fileName.find_last_of(".") + 1) == "zip")
 		{
-			printf("Successfully checked .zip file\n");
 			pf = fopen (argv[1], "r");
 			if (pf == NULL)
 				{cout  << "Error opening file\n"; return 0;}
-			else
-				{cout << "File opened\n";}
 		}
 		 else
 		 {printf("Error: File Extension\n"); return 0;}
