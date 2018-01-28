d80c2f0258fbcf6811eac74df84010e78a883ba3
thekevintran <32287363+thekevintran@users.noreply.github.com>
2018-01-28 17:11:40 -0500

    Update unzip_file_ext_check.cpp

--- unzip_file_ext_check.cpp
+++ unzip_file_ext_check.cpp
@@ -13,23 +13,22 @@ int main (int argc, char* argv[])
 		printf("Error: More than 1 argument found\n");
 		return 0;
 	}
-  
-	string fileName = argv[1];
-  	if(fileName.substr(fileName.find_last_of(".") + 1) == "zip")
-  	{
-		printf("Successfully checked .zip file\n");
-		return 0;
-  	}
- 	 else
- 	 {
-		printf("Error: File Extension\n");
- 	 }
-	pf = fopen (argv[1], "r");
-
-	if (pf == NULL)
-	{cout  << "Error opening file\n";}
-	else
-	{cout << "File opened\n";}
+  	else
+	{
+		string fileName = argv[1];
+		if(fileName.substr(fileName.find_last_of(".") + 1) == "zip")
+		{
+			printf("Successfully checked .zip file\n");
+			return 0;
+			pf = fopen (argv[1], "r");
+			if (pf == NULL)
+			{cout  << "Error opening file\n"; return 0;}
+			else
+			{cout << "File opened\n";}
+		}
+		 else
+		 {printf("Error: File Extension\n"); return 0;}
+	}
   
   return 0;
   }
